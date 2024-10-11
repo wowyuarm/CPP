219fce397652166e279d01de19532e602a61025c
wowyuarm <wuyuang05@outlook.com>
2024-10-11 21:08:16 +0800

    C++-2

---
+++ CPP2.c
@@ -0,0 +1,28 @@
+//三位数中的水仙花数个数 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num=100;
+	int a,b,c;
+	
+	do
+	{
+	   a=num/100;
+	   b=num/10%10;
+	   c=num%10;
+	   
+	   if(num==a*a*a+b*b*b+c*c*c)
+      {
+		cout<<num<<endl;
+		num++;
+ 	  }
+	   else
+	  {
+		num++;
+	  }
+    }while(num<1000);
+	
+	return 0;
+}
