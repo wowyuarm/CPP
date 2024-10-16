8bb66680432079e5b15982b2ade5e85ebade4153
wowyuarm <wuyuang05@outlook.com>
2024-10-16 21:18:05 +0800

    C++-4

---
+++ CPP4.c
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	int c=4;
+	double x=0.5;
+	double pi=3.14159;
+	double a,b;
+	
+	a=sqrt(pow(sin(x),2.5));
+	b=pow(c,x*x)/sqrt(2*pi);
+	cout<<a<<endl;
+	cout<<b<<endl;
+	
+	return 0;
+}
+
